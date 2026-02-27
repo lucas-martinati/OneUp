a6ff4662ace5e70cf62e25230a6c69341eed50d0
Lucas_M54 <lucasm54800@gmail.com>
2026-02-27 20:09:50 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/OneUp/sw.js', { scope: '/OneUp/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
