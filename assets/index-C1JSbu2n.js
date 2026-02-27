12534669655885be89129567ab965ded08808240
Lucas_M54 <lucasm54800@gmail.com>
2026-02-27 21:35:46 +0100

    Updates

---
+++ assets/index-C1JSbu2n.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-8KXfjER-.js","assets/index-Df-yH3RT.js","assets/index-CxOrEPyn.css"])))=>i.map(i=>d[i]);
+import{r as t,_ as n}from"./index-Df-yH3RT.js";var e;(function(r){r.Heavy="HEAVY",r.Medium="MEDIUM",r.Light="LIGHT"})(e||(e={}));var i;(function(r){r.Success="SUCCESS",r.Warning="WARNING",r.Error="ERROR"})(i||(i={}));const a=t("Haptics",{web:()=>n(()=>import("./web-8KXfjER-.js"),__vite__mapDeps([0,1,2])).then(r=>new r.HapticsWeb)});export{a as Haptics,e as ImpactStyle,i as NotificationType};
