25af319d0974bb26e49ab8b49edcb93afe197036
Lucas_M54 <lucasm54800@gmail.com>
2026-03-16 19:01:15 +0100

    Updates

---
+++ assets/index-CCTJiksn.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-VY7r-vKz.js","assets/index-DNFjKCVn.js","assets/index-Hg9e-Z-T.css"])))=>i.map(i=>d[i]);
+import{r as t,_ as n}from"./index-DNFjKCVn.js";var e;(function(r){r.Heavy="HEAVY",r.Medium="MEDIUM",r.Light="LIGHT"})(e||(e={}));var i;(function(r){r.Success="SUCCESS",r.Warning="WARNING",r.Error="ERROR"})(i||(i={}));const a=t("Haptics",{web:()=>n(()=>import("./web-VY7r-vKz.js"),__vite__mapDeps([0,1,2])).then(r=>new r.HapticsWeb)});export{a as Haptics,e as ImpactStyle,i as NotificationType};
