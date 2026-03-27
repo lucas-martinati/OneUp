70a951587a4664faf544711514f00d73623912eb
Lucas_M54 <lucasm54800@gmail.com>
2026-03-27 08:15:03 +0100

    feat: Enable WebView debugging for Android KitKat and above.

--- android/app/src/main/java/com/lucasm548/oneup/MainActivity.java
+++ android/app/src/main/java/com/lucasm548/oneup/MainActivity.java
@@ -1,5 +1,19 @@
 package com.lucasm548.oneup;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+    }
+}
