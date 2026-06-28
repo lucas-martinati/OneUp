34aaf029a161ce72f9506acf98ed6a041177d9b5
Lucas_M54 <lucasm54800@gmail.com>
2026-06-28 15:51:30 +0200

    Updates

---
+++ assets/useComputedStatsFromStore-DfcyYnJF.js
@@ -0,0 +1,1 @@
+import{y as o}from"./index-rSjKql93.js";function r(){return o(t=>t.stats)}export{r as u};
--- assets/useComputedStatsFromStore-eRaCGgcy.js
+++
@@ -1,1 +0,0 @@
-import{y as o}from"./index-DjilIzUG.js";function r(){return o(t=>t.stats)}export{r as u};
